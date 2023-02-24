24c1e9a461365092d1a39094d07f7bb9f111b53d
Federico <fcucci18@gmail.com>
2023-02-24 21:37:32 +0100

    change getClinicExamsbyPatient in contract

--- clinic-project-hardhat/contracts/ClinicContract.sol
+++ clinic-project-hardhat/contracts/ClinicContract.sol
@@ -13,10 +13,9 @@ struct ClinicExam{
     string report;
     uint256 date;
     bool isFile;
-    bool isConfirmed;
 }
 
-event ClinicExamAdded(address doctor,address patient, string examType, string report, uint256 date, bool isFile, bool isConfirmed);
+event ClinicExamAdded(address doctor,address patient, string examType, string report, uint256 date, bool isFile);
 event ClinicExamConfirmed(string message, ClinicExam e);
 
 mapping (uint => ClinicExam) private mapExam;
@@ -54,10 +53,10 @@ function addClinicExam(address patient, string memory examType, string memory re
         require(existDoctor(msg.sender),"You're not a doctor!");
         uint idExam=idCount;
         uint256 date=block.timestamp;
-        mapExam[idExam]=ClinicExam(msg.sender,patient,examType,report,date,isFile,false);
+        mapExam[idExam]=ClinicExam(msg.sender,patient,examType,report,date,isFile);
         idCount+=1;
         mapConfirmExam[idExam][msg.sender] = true;
-        emit ClinicExamAdded(msg.sender, patient, examType, report, date, isFile,false);
+        emit ClinicExamAdded(msg.sender, patient, examType, report, date, isFile);
         return idExam;
     }
 
@@ -66,11 +65,10 @@ function addClinicExam(address patient, string memory examType, string memory re
 function confirmClinicExam(uint256 idExam) public{
     require(mapExam[idExam].patient==msg.sender,"You're not a patient");
     mapConfirmExam[idExam][msg.sender] = true;
-    mapExam[idExam].isConfirmed=true;
     emit ClinicExamConfirmed("Patient and doctor both confirmed exam",mapExam[idExam]);
 }
 
-function getClinicExamsByPatient(address patient) public view returns(ClinicExam[] memory, bool[] memory, bool[] memory){
+function getClinicExamsByPatient(address patient) public view returns(ClinicExam[] memory, bool[] memory, uint256[] memory){
     uint256 count=0;
     for(uint256 i=0;i<idCount;i++){
         if(mapExam[i].patient==patient){
@@ -78,19 +76,19 @@ function getClinicExamsByPatient(address patient) public view returns(ClinicExam
         }
     }
     ClinicExam [] memory exams=new ClinicExam[](count);
-    bool[] memory isFileList= new bool[](count);
     bool[] memory isConfirmedList= new bool[](count);
+    uint256[] memory listID= new uint256[](count);
     uint256 j=0;
     for(uint256 i=0;i<idCount;i++){
         if(mapExam[i].patient==patient){
             exams[j]=mapExam[i];
-            isFileList[j]=mapExam[i].isFile;
-            isConfirmedList[j]=mapExam[i].isConfirmed;
+            isConfirmedList[j]=isConfirmed(i,patient);
+            listID[j]=i;
             j++;
         }
     }
     
-    return (exams,isFileList,isConfirmedList);
+    return (exams,isConfirmedList,listID);
    
 }
 
